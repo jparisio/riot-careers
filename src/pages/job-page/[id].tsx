ff6fb179326262ffe3b1b29523d9c372f801f3ea
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-06-13 09:29:21 -0400

    cool image thing

--- src/pages/job-page/[id].tsx
+++ src/pages/job-page/[id].tsx
@@ -63,9 +63,12 @@ export default function Page({ job }: { job: JobType }) {
 
         <motion.img
           initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.75, delay: 0.3 }}
-          exit={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 0.75, delay: 0.3 },
+          }}
+          exit={{ opacity: 0, transition: { duration: 0.5 } }}
           src="/riot-sketch.webp"
           alt="Riot Games Logo"
           width={800}
